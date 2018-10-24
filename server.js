e762001176413bad70a8afddb8236ff780fd54df
TT <tinainink@gmail.com>
2018-10-24 08:49:56 +0530

    Tina | Simulate CPU time for performance testing

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs')
 const PORT = process.env.PORT || 3000;
 
 const server = function() {
   const app = express()
   app.get('/', (req, res) => {
     res.send('Hello World!');
+    for(i=0; i<1234e4; i++);   
     console.log(`Handled by process ${process.pid}`);
   });
   
